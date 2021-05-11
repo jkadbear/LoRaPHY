fd1cf81058f29430879e9b061f841c6d41b54705
jkadbear <jkadbear@gmail.com>
2021-05-11 20:58:10 +0800

    Add function spec

--- LoRaPHY.m
+++ LoRaPHY.m
@@ -766,5 +766,25 @@ classdef LoRaPHY < handle
 
             y = cat(2, c1(1:snum-1), c2).';
         end
+
+        function spec(sig, fs, bw, sf)
+            x = 0:1/sf:2^sf/bw;
+            y = -bw/2:bw/2;
+            p = fs/bw;
+            win_length = 2^(sf-2);
+            N = p*2^sf;
+            s = spectrogram(sig,win_length,round(win_length*0.8),N);
+            valid_data_len = round(2^sf/2*1.5);
+            b = abs(s(valid_data_len:-1:1,:));
+            c = abs(s(end:-1:end-valid_data_len+1,:));
+            d = [b;c];
+            figure;
+            imagesc(x,y,d);
+            colormap summer;
+            title('Spectrogram');
+            xlabel('Time');
+            ylabel('Frequency');
+            set(gcf,'unit','normalized','position',[0.05,0.2,0.9,0.1]);
+        end
     end
 end
